1890427e42922e9c4da333c960b8072c582ede9c
ProphesierC <bwknowles98@gmail.com>
2024-09-14 05:13:33 -0400

    Initialized Vue3 + Vite + Tailwindcss project

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,17 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1 class="text-3xl font-h2 font-bold text-center text-gray-800 dark:text-gray-200">
+    Hello world!
+  </h1>
 </template>
 
 <style scoped>
