0a9acb9a7461240bdc554be9de35111870c82371
ProphesierC <bwknowles98@gmail.com>
2024-09-15 01:14:39 -0400

    Installed Vuetify / Vite dependencies

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,22 +2,9 @@
 </script>
 
 <template>
-  <h1 class="text-3xl font-h2 font-bold text-center text-gray-800 dark:text-gray-200">
-    Hello world!
-  </h1>
+  
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
