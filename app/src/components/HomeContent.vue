0dc822ecaf0e2642d55d15d2c906b5bbec39bc5a
ProphesierC <bwknowles98@gmail.com>
2024-09-26 22:40:25 -0400

    update app/index.html, HomeContent.vue and style.css added Bellefair custom font

--- app/src/components/HomeContent.vue
+++ app/src/components/HomeContent.vue
@@ -2,9 +2,8 @@
     <div class="absolute text-white w-100 top-24 text-center" 
     style="font-family: var(--ff-barlow-condensed);">
         <h1>SO, YOU WANT TO TRAVEL TO</h1>
-        <h2 class="text-[104px] tracking-widest" 
-        style="font-family: var(--ff-barlow-condensed);">Space</h2>
-        <!-- needs custom font -->
+        <h2 class="text-[80px] tracking-widest" 
+        style="font-family: var(--ff-bellefair);">SPACE</h2>
         <p>
             Let’s face it; if you want to go to space, you might as well genuinely go to 
             outer space and not hover kind of on the edge of it. Well sit back, and relax 
