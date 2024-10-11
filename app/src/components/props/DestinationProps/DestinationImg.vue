22c131cc3843d02a81791894122b06e228034000
ProphesierC <bwknowles98@gmail.com>
2024-10-10 20:00:39 -0400

    update DestinationImg.vue and style.css

--- app/src/components/props/DestinationProps/DestinationImg.vue
+++ app/src/components/props/DestinationProps/DestinationImg.vue
@@ -4,26 +4,37 @@
     v-for="(destination, index) in postData" 
     :key="index">
     <keep-alive>
+
       <v-img
         v-if="index === props.index"
         :width="200"
         :src="destination.images.png" 
         alt="Destination Image"
       >
-      <template v-slot:placeholder>
-        <div 
-          class="d-flex align-center justify-center fill-height">
+        <template v-slot:placeholder>
+          <div 
+            class="flex align-center justify-center fill-height">
 
-          <v-progress-circular
-            color="amber"
-            indeterminate
-          ></v-progress-circular>
+            <v-progress-circular
+              color="amber"
+              indeterminate
+            ></v-progress-circular>
 
-        </div>
-      </template>
+          </div>
+        </template>
 
       </v-img>
     </keep-alive>
+
+      <div class="flex h-[20vh] flex-col">
+        <p 
+          v-if="index === props.index"
+          class="flex text-white flex-col h-[0] items-center mt-auto"
+          >{{destination.description}}
+        </p>
+        <!-- try to anchor to drawer for better relative positioning -->
+      </div>
+
   </div>
 </template>
 
