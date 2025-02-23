2d5cdf19f52e379c7ecdee48038d4700b2be035f
ProphesierC <bwknowles98@gmail.com>
2025-02-23 17:45:21 -0500

    build(deps): update package-lock.json, package.json and vite.config.js

--- app/vite.config.js
+++ app/vite.config.js
@@ -13,5 +13,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src')
     }
   },
-  base: '/space-tourism-website-main/' 
+  base: '/', // For dev, set base to '/' to avoid issues
 })
